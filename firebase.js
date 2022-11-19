1ab73a0396df6d0545205f1abec41de26a5d91bf
Elliott Sánchez León <raptordroid15@gmail.com>
2022-11-19 12:10:52 -0600

    Listar datos parte 3

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
+  import { getFirestore, collection, addDoc, getDocs, onSnapshot } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-firestore.js"
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,3 +22,7 @@
   export const saveTask = (title, description) => {
     addDoc(collection(db, 'tasks'), {title, description})
   }
+
+  export const getTasks = () => getDocs(collection(db, 'tasks'))
+
+  export const onGetTasks = () => onSnapshot(collection(db, 'tasks'), callback)
