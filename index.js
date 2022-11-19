19f45274c0eb398bf3f8d39edba537a83aa62218
Elliott Sánchez León <raptordroid15@gmail.com>
2022-11-19 11:14:02 -0600

    Firestore setup parte 2

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.14.0/firebase-app.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import {saveTask} from './firebase.js'
 
-  // Your web app's Firebase configuration
-  const firebaseConfig = {
-    apiKey: "AIzaSyDVip1EGmc-jp0S5-H3GkYxttnXzPvgYGY",
-    authDomain: "fir-javascript-crud-3e398.firebaseapp.com",
-    projectId: "fir-javascript-crud-3e398",
-    storageBucket: "fir-javascript-crud-3e398.appspot.com",
-    messagingSenderId: "10830739018",
-    appId: "1:10830739018:web:061d84b1f6e49a299498b3"
-  };
+window.addEventListener('DOMContentLoaded', () => {
+  
+});
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
+const taskForm = document.getElementById('task-form')
+
+taskForm.addEventListener('submit', (e) => {
+  e.preventDefault()
+
+  const title = taskForm['task-title']
+  const description = taskForm['task-description']
+
+  saveTask(title.value, description.value)
+
+  taskForm.reset()
+
+})
+
